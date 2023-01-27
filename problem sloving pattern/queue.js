28f7814fddad5bf336ab8abc6aff8dbd7699f149
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2023-01-28 00:18:21 +0530

    queue implementation enqueue dequeue

---
+++ problem sloving pattern/queue.js
@@ -0,0 +1,20 @@
+var Queue = function () {
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function (key, value) {
+  this.storage[key] = value;
+};
+
+Queue.prototype.dequeue = function () {
+  const first = Object.keys(this.storage)[0];
+  delete this.storage[first];
+};
+
+Queue.prototype.size = function () {
+  return this.storage;
+};
+
+var myWeeklyMenu = new Queue();
+
+myWeeklyMenu.enqueue("name", "raju");
