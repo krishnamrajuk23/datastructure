00f5ab7070f497ea22964d2efedcf0642d3ae8e1
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2023-02-11 18:16:51 +0530

    stack with object example

--- problem sloving pattern/stack.js
+++ problem sloving pattern/stack.js
@@ -1,3 +1,5 @@
+/*Stack with string example*/
+
 var Stack = function () {
   this.storage = "";
 };
@@ -24,3 +26,29 @@ Stack.prototype.size = function () {
 var myWeeklyMenu = new Stack();
 
 myWeeklyMenu.push("RedBeans");
+
+
+
+/*Stack with object example*/
+
+var StackWithObj = function (capacity) {
+  this.storage = {};
+  this.count = 0;
+  this.capacity = capacity || Infinity;
+}
+
+StackWithObj.push = function (value) {
+  if (this.count <= this.capacity) {
+    this.storage[this.count++] = value;
+  }
+
+  throw new Error("stack is fill you can't push more than capacity");
+}
+
+StackWithObj.pop = function () {
+  if (this.count !== 0) {
+    delete this.storage[--this.count];
+  }
+  throw new Error("stack is empty");
+}
+
