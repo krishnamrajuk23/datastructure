be71893e146413d1678d66c51c069a8673e9bd29
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2023-05-01 13:00:08 +0530

    string solving patterns

---
+++ problem sloving pattern/string/find-reepeating-character.js
@@ -0,0 +1,20 @@
+// Maximum repeating character for every index in given String
+
+function findLargest(str) {
+  let arrWithZeroValue = new Array(126).fill(0);
+  let max = 0, charMax = '';
+  for (let i = 0; i < str.length; i++) {
+    let ch = str[i].charCodeAt(0);
+    arrWithZeroValue[ch]++
+    console.log("check the updated arrWithZeroValue array", ch, arrWithZeroValue[ch]++);
+    if (arrWithZeroValue[ch] > max) {
+
+      max = arrWithZeroValue[ch];
+      charMax = ch;
+    }
+  }
+
+  console.log("maximum repeated character is " + String.fromCharCode(charMax) + "and count is" + max);
+}
+
+findLargest("parallel"); // Output: 'l' ---> letter l;
